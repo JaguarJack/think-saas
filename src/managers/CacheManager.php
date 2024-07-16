a8f4e76f16a570788185084a0386330c12136cbe
JaguarJack <njphper@gmail.com>
2024-07-16 09:08:19 +0800

    feat:缓存&cookie兼容多租户

--- src/managers/CacheManager.php
+++ src/managers/CacheManager.php
@@ -1,7 +1,57 @@
 <?php
 namespace think\saas\managers;
 
-class CacheManager extends \think\Cache
-{
+use think\Cache;
+use think\saas\support\Tenant;
 
+class CacheManager extends Cache
+{
+    /**
+     * 获取缓存配置
+     * @access public
+     * @param null|string $name    名称
+     * @param mixed       $default 默认值
+     * @return mixed
+     */
+    public function getConfig(string $name = null, $default = null): mixed
+    {
+        if (!is_null($name)) {
+            return $this->addTenantPrefix($this->app->config->get('cache.' . $name, $default));
+        }
+
+        return $this->addTenantPrefix($this->app->config->get('cache'));
+    }
+
+    /**
+     * 添加租户前缀
+     *
+     * @param $config
+     * @return mixed
+     */
+    protected function addTenantPrefix($config): mixed
+    {
+        $tenantPrefix = $this->app->get(Tenant::class)->tenantPrefix();
+
+        if (isset($config['stores'])) {
+            foreach ($config['stores'] as &$store) {
+                $store['prefix'] = $this->addPrefixIfNotSet($tenantPrefix, $store['prefix']);
+                $store['tag_prefix'] = $this->addPrefixIfNotSet($tenantPrefix, $store['tag_prefix']);
+            }
+        } else {
+            $config['prefix'] = $this->addPrefixIfNotSet($tenantPrefix, $config['prefix']);
+            $config['tag_prefix'] = $this->addPrefixIfNotSet($tenantPrefix, $config['tag_prefix']);
+        }
+
+        return $config;
+    }
+
+    /**
+     * @param $tenantPrefix
+     * @param $prefix
+     * @return string
+     */
+    protected function addPrefixIfNotSet($tenantPrefix, $prefix): string
+    {
+        return $prefix ? ($tenantPrefix . ':' . $prefix) : $tenantPrefix;
+    }
 }
---
+++ src/managers/CookieManager.php
@@ -0,0 +1,66 @@
+<?php
+namespace think\saas\managers;
+
+use think\Cookie;
+use think\saas\support\Tenant;
+
+class CookieManager extends Cookie
+{
+
+    /**
+     * 获取cookie
+     * @access public
+     * @param  mixed  $name 数据名称
+     * @param  string $default 默认值
+     * @return mixed
+     */
+    public function get(string $name = '', $default = null)
+    {
+        return $this->request->cookie($this->getCookieName($name), $default);
+    }
+
+    /**
+     * 是否存在Cookie参数
+     * @access public
+     * @param  string $name 变量名
+     * @return bool
+     */
+    public function has(string $name): bool
+    {
+        return $this->request->has($this->getCookieName($name), 'cookie');
+    }
+
+    /**
+     * Cookie 保存
+     *
+     * @access public
+     * @param  string $name  cookie名称
+     * @param  string $value cookie值
+     * @param  int    $expire 有效期
+     * @param  array  $option 可选参数
+     * @return void
+     */
+    protected function setCookie(string $name, string $value, int $expire, array $option = []): void
+    {
+        $this->cookie[$this->getCookieName($name)] = [$value, $expire, $option];
+    }
+
+    /**
+     * @param $name
+     * @return string
+     */
+    protected function getCookieName($name): string
+    {
+        return $this->getTenantPrefix() . ':' . $name;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTenantPrefix(): string
+    {
+        $tenant = app()->make(Tenant::class);
+
+        return $tenant->tenantPrefix();
+    }
+}
